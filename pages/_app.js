63ede4900fbbc9f7accb2fe90ca5423d6e160c7f
Luis C <me@lucataco.com>
2023-02-13 01:00:04 -0500

    Plausible Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
+import Script from 'next/script'
 import "../styles/globals.css";
 import "../styles/app.css";
 import "../styles/normal.css";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  <>
+    <Script defer data-domain="chatgpt3sandbox.vercel.app" src="https://data.lucata.co/js/script.js" />
+    <Component {...pageProps} />
+  </>
+  );
 }
